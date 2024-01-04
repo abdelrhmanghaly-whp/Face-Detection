8413f51f60a6e2a7ab0933cfaaca31b7ccb81960
Abdelrhman Ghaly <84441904+abdelrhmanghaly-whp@users.noreply.github.com>
2024-01-04 16:38:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import face_recognition
 import os
 from datetime import datetime
 
-
 path = 'imgs'
 images = []
 Names = []
@@ -18,21 +17,22 @@ for i in myList:
 
 print(Names)
 
-
 def findEncodings(imgs):
     encodedList = []
     for i in imgs:
         i = cv2.cvtColor(i, cv2.COLOR_BGR2RGB)
-        encoded = face_recognition.face_encodings(i)[0]
-        encodedList.append(encoded)
-    return encodedList
+        try:
+            encoded = face_recognition.face_encodings(i)[0]
+            encodedList.append(encoded)
+        except IndexError:
+            print(f"No face found in image: {i}")
 
+    return encodedList
 
 def markAttend(name):
     with open('attendance.csv', 'r+') as ar:
         dataList = ar.readlines()
         namesList = []
-       # print(dataList)
         for i in dataList:
             entry = i.split(',')
             namesList.append(entry[0])
@@ -41,8 +41,12 @@ def markAttend(name):
             converted_dt_str = curr.strftime('%H:%M:%S')
             ar.writelines(f'\n{name},{converted_dt_str}')
 
-
 known = findEncodings(images)
+
+if not known:
+    print("No faces found in the provided images. Exiting.")
+    exit()
+
 print("Encoding Completed!")
 
 cap = cv2.VideoCapture(0)
@@ -56,12 +60,10 @@ while True:
     for currFace, loc in zip(currEncoding, frames):
         matches = face_recognition.compare_faces(known, currFace)
         dist = face_recognition.face_distance(known, currFace)
-        # print(dist)
         idx = np.argmin(dist)
 
         if matches[idx]:
             name = Names[idx].upper()
-           # print(name)
             y1, x2, y2, x1 = loc
             y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
@@ -71,4 +73,5 @@ while True:
             markAttend(name)
 
     cv2.imshow('Camera', img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) == 27:
+        break
